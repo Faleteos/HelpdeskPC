441644a9d8755f1e521317817f39d18356d4973b
unknown <fltellezo@itc.edu.co>
2023-02-23 00:27:28 -0500

    Helpdesk version 1

---
+++ src/app/Components/ingresopersonas/ingresopersonas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresopersonas',
+  templateUrl: './ingresopersonas.component.html',
+  styleUrls: ['./ingresopersonas.component.css']
+})
+export class IngresopersonasComponent {
+
+}
