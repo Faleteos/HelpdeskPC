acaddec44defa406fec4a3a0876c527f92990ccb
unknown <fltellezo@itc.edu.co>
2023-04-23 23:33:34 -0500

    conexion back

---
+++ src/app/Components/informes/informes.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ApiService } from 'src/app/Services/api.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-informes',
+  templateUrl: './informes.component.html',
+  styleUrls: ['./informes.component.css']
+})
+export class InformesComponent implements OnInit{
+  column:Object;
+  displayedColumns: string[]=[]
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;  
+  constructor(public Api: ApiService){
+    this.dataSource=new MatTableDataSource();
+  }
+  ngOnInit(): void {
+    this.GetRepuestos();
+  //  throw new Error('Method not implemented.');
+  }
+
+ public async GetRepuestos(){
+  //var response=this.Api.Get("Personas");
+  await this.Api.get("Informes").then((res)=>{
+
+    this.displayedColumns=Object.keys(res[0])
+      // this.loadTable([res[0]])
+      this.dataSource.data=res
+ });
+ this.dataSource.paginator=this.paginator;
+ this.dataSource.sort=this.sort
+
+}
+applyFilter(event: Event) {
+  const filterValue = (event.target as HTMLInputElement).value;
+  this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  if (this.dataSource.paginator) {
+    this.dataSource.paginator.firstPage();
+  }
+}
+
+}
